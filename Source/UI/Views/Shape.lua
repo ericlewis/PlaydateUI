0e2687601946c1e4dccaa8a5cc56d46d9a43cb15
Eric Lewis <ericlewis777@gmail.com>
2022-03-09 06:16:04 -0500

    add RoundedRectangle, Circle primitives

--- Source/UI/Views/Shape.lua
+++ Source/UI/Views/Shape.lua
@@ -3,10 +3,20 @@ import "CoreLibs/graphics"
 
 local gfx <const> = playdate.graphics
 
--- public
 _LeafView('Rectangle', { isLeaf = true })
-
---internal
 function Rectangle:draw()
     gfx.fillRect(self:_getComputedLayout())
+end
+
+_LeafView('RoundedRectangle', { isLeaf = true })
+function RoundedRectangle:init(cornerRadius)
+    self.cornerRadius = cornerRadius or 3
+end
+function RoundedRectangle:draw()
+    gfx.fillRoundRect(self:_getComputedLayout(), self.cornerRadius)
+end
+
+_LeafView('Circle', { isLeaf = true })
+function Circle:draw()
+    gfx.fillCircleInRect(self:_getComputedLayout())
 end
