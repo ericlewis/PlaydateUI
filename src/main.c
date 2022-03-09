7b00c4d11841c5dc966ae8b1db50cb9021d842ff
Eric Lewis <ericlewis777@gmail.com>
2022-03-09 05:43:57 -0500

    more clean

--- src/main.c
+++ src/main.c
@@ -1,16 +1,9 @@
 #include "pd_api.h"
 #include "yoga_bindings.h"
 
-PlaydateAPI *pd = NULL;
-
-#ifdef _WINDLL
-__declspec(dllexport)
-#endif
-
 int eventHandler(PlaydateAPI *playdate, PDSystemEvent event, uint32_t arg) {
     if (event == kEventInitLua) {
-        pd = playdate;
-        registerYoga(pd);
+        registerYoga(playdate);
     }
     return 0;
 }
