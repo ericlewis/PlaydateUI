bea64942ae618863b78f8c92c76bb31a477e7648
Eric Lewis <ericlewis777@gmail.com>
2022-03-09 20:09:43 -0500

    lay groundwork for handling layout alignments

--- Source/UI/Views/Layout.lua
+++ Source/UI/Views/Layout.lua
@@ -4,16 +4,34 @@ local flexDirectionColumn <const> = 0
 local flexDirectionRow <const> = 2
 
 View '_Layout'
-function _Layout:init(children)
+function _Layout:init(isHorizontal, children)
+    self.isHorizontal = isHorizontal
     self.children = children or {}
 end
-
-class('VStack').extends(_Layout)
-function VStack:_transformNode(node)
-    node:setFlexDirection(flexDirectionColumn)
+function _Layout:_transformNode(node)
+    if self.isHorizontal then
+        node:setFlexDirection(flexDirectionRow)
+    else
+        node:setFlexDirection(flexDirectionColumn)
+    end
 end
 
-class('HStack').extends(_Layout)
-function HStack:_transformNode(node)
-    node:setFlexDirection(flexDirectionRow)
+function VStack(props)
+    if type(props) == "table" then
+        return _Layout(false, props)
+    end
+
+    return function(children)
+        return _Layout(false, children)
+    end
+end
+
+function HStack(props)
+    if type(props) == "table" then
+        return _Layout(true, props)
+    end
+
+    return function(children)
+        return _Layout(true, children)
+    end
 end
